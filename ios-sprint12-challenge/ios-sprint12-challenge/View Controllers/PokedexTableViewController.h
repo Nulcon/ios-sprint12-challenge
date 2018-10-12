dad236e6ee00453909ee4c17db78eb865f381756
Conner Alegre <cga0@pm.me>
2018-10-12 09:24:53 -0600

    Reorganize and setup base layout

--- ios-sprint12-challenge/ios-sprint12-challenge/View Controllers/PokedexTableViewController.h
+++ ios-sprint12-challenge/ios-sprint12-challenge/View Controllers/PokedexTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  PokedexTableViewController.h
 //  ios-sprint12-challenge
 //
 //  Created by Conner on 10/12/18.
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+NS_ASSUME_NONNULL_BEGIN
 
+@interface PokedexTableViewController : UITableViewController
 
 @end
 
+NS_ASSUME_NONNULL_END
