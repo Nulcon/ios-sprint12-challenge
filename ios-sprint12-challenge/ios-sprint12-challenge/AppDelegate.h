ba2b65462ac4e3ecef0c359bcc2c829e9bd7660e
Conner Alegre <cga0@pm.me>
2018-10-12 09:08:40 -0600

    initial commit

---
+++ ios-sprint12-challenge/ios-sprint12-challenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-sprint12-challenge
+//
+//  Created by Conner on 10/12/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
